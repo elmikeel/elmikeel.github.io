c45b04c1aa2dc6a3c8df9125cbfc65509b16b99f
sifudiep <mikael.diep@gmail.com>
2018-12-05 15:10:19 +0100

    Updates

---
+++ precache-manifest.0bb9581d679454a0ce9c061e2a680466.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c148b7cbab2e9ca1d020",
+    "url": "/static/js/main.c148b7cb.chunk.js"
+  },
+  {
+    "revision": "4143f3b5e015d44ffe24",
+    "url": "/static/js/1.4143f3b5.chunk.js"
+  },
+  {
+    "revision": "c148b7cbab2e9ca1d020",
+    "url": "/static/css/main.1a49607b.chunk.css"
+  },
+  {
+    "revision": "f2474b9993067ff6cb60557afa1e253f",
+    "url": "/index.html"
+  }
+];
